6e70247e4f30fde4c402670d51b69615712ac103
howellad <adster.howell@gmail.com>
2024-03-30 16:31:50 +0000

    enable ssr build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   css: [
     '@fortawesome/fontawesome-free/css/all.css'
   ],
