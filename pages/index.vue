c7055ed3cee73bb123ce1f58d1f0046ca02b067f
Adam Howell <86600061+howellad@users.noreply.github.com>
2024-06-07 12:41:50 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center">
         <text class="font-bold text-2xl mt-8 ml-1 mr-1 text-center">
-            Welcome to Darlingtons finest Swim team!
+            Welcome to Darlington Masters Swim team!
         </text>
     </div>
 </template>
