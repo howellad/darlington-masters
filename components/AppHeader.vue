aef14c18969171daa92b3710d5ee08906651697b
howellad <adster.howell@gmail.com>
2024-06-20 03:51:39 +0200

    fixing preview

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <a class="flex sm:justify-center sm:items-center sm:px-4 sm:py-3" href="/">
-        <img class="m-4 object-none" src='@/assets/badge2.png' />
+        <img class="m-4 object-none" src='@/assets/logo.png' />
         <div class="text-3xl p-6 font-semibold">
             Darlington Dolphin Masters ASC
         </div>
