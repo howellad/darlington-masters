9a3c432b9898b60e1f2fbcf8f9a2367347047934
howellad <adster.howell@gmail.com>
2024-07-18 22:10:10 +0100

    updating the card to have images passed as a prop

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const items = [
-    'https://picsum.photos/600/800?random=1',
+    'assets/Main_Pool_1.jpg',
     'https://picsum.photos/600/800?random=2',
     'https://picsum.photos/600/800?random=3',
     'https://picsum.photos/600/800?random=4',
@@ -11,11 +11,14 @@ const items = [
 
 <template>
     <div class="flex justify-center">
-        <text class="font-bold text-2xl mt-8 ml-1 mr-1 text-center">
+        <div>
+            <text class="font-bold text-2xl mt-8 ml-1 mr-1 text-center">
             Welcome to Darlington Masters Swim team!
-        </text>
-        <UCarousel v-slot="{ item }" :items="items">
-            <img :src="item" width="300" height="400" draggable="false">
-        </UCarousel>
+            </text>
+            <UCarousel v-slot="{ item }" :items="items">
+                <img :src="item" width="300" height="400" draggable="false">
+            </UCarousel>
+            <img  src="assets/Main_Pool_1.jpg">
+        </div>
     </div>
 </template>
